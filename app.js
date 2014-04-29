1e6ba1372bce02bc7ee69b7b560b31d4731e7dc5
Dixen Cheng (RD-TW) <dixentw@gmail.com>
2014-04-29 21:22:46 +0800

    add facebook login button

--- app.js
+++ app.js
@@ -1,16 +1,34 @@
 var express = require("express");
 var logfmt = require("logfmt");
+var fs = require("fs");
+var https = require('https');
+
 var app = express();
+
+var options = {
+  key:  fs.readFileSync(__dirname + '/keys/key.pem'),
+  cert: fs.readFileSync(__dirname + '/keys/key-cert.pem')
+};
+
+https.createServer(options, app).listen(443);
 
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-  res.send(__dirname + "/public/index.html");
+	var page = __dirname + "/public/index.html" 
+	fs.readFile(page, function (err, data) {
+		if (err) {
+	  		res.writeHead(500);
+	 		return res.end('Error loading index.html');
+ 	    }else{
+ 	    	res.writeHead(200);
+	     	res.end(data);
+	     	//res.send(page);
+ 		}
+ 	});
 });
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
-
-console.log("testing push");
