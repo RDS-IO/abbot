e751c71b34e5c446af6b1a53606d89ffe667970b
Dixen Cheng (RD-TW) <dixentw@gmail.com>
2014-05-26 13:36:22 +0800

    before refactor to module FB.api()

--- app.js
+++ app.js
@@ -5,30 +5,13 @@ var https = require('https');
 
 var app = express();
 
-/*
-var options = {
-  key:  fs.readFileSync(__dirname + '/keys/key.pem'),
-  cert: fs.readFileSync(__dirname + '/keys/key-cert.pem')
-};
-https.createServer(options, app).listen(443);*/
 app.set('views', __dirname+'/public');
 app.engine('html', require('ejs').renderFile);
 app.use(logfmt.requestLogger());
+app.use('/js', express.static(__dirname + '/public/js'));
 
 app.get('/', function(req, res) {
 	res.render('index.html');
-	/*
-	var page = __dirname + "/public/index.html" 
-	fs.readFile(page, function (err, data) {
-		if (err) {
-	  		res.writeHead(500);
-	 		return res.end('Error loading index.html');
- 	    }else{
- 	    	res.writeHead(200);
-	     	res.end(data);
-	     	//res.send(page);
- 		}
- 	});*/
 });
 
 var port = Number(process.env.PORT || 5000);
